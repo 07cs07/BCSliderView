bcfbd825201594144ffb482b77ceefd8c6fe8d90
Balachander <balachander@mobsandgeeks.com>
2013-07-31 11:15:45 +0530

    Initial Commit

---
+++ BCSliderDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BCSliderDemo
+//
+//  Created by MaG~2 on 31/07/13.
+//  Copyright (c) 2013 Mobs and Geeks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BCSliderDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BCSliderDemo
+//
+//  Created by MaG~2 on 31/07/13.
+//  Copyright (c) 2013 Mobs and Geeks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
